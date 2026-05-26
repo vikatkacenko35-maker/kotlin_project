abdbdc178dc1d6383c3cfc33ae46c8d34a5810b4
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-05-27 01:56:07 +0300

    made movie picjer

--- app/src/main/java/com/tkachenko/final_project/MovieViewModel.kt
+++ app/src/main/java/com/tkachenko/final_project/MovieViewModel.kt
@@ -1,2 +1,17 @@
 package com.tkachenko.final_project
 
+import androidx.compose.runtime.mutableStateListOf
+
+class MovieViewModel {
+
+    val movieList = mutableStateListOf<Movie>()
+
+    fun addMovie(movie: Movie) {
+        movieList.add(movie)
+    }
+
+    fun getRandomMovie(): Movie? {
+        if (movieList.isEmpty()) return null
+        return movieList.random()
+    }
+}
