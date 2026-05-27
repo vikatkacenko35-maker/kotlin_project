479c80dbd1ca37134632bc98f9c0f6e56280e624
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-05-27 03:23:24 +0300

    add internetgit add .!

--- app/src/main/java/com/tkachenko/final_project/Movie.kt
+++ app/src/main/java/com/tkachenko/final_project/Movie.kt
@@ -5,3 +5,6 @@ data class Movie(
     val description: String,
     val imageUrl: String
 )
+//https://image.fonwall.ru/o/kp/spiderman-ps4-spiderman-games-2018-games-yipi.jpeg?auto=compress&fit=crop&w=1920&h=1080&domain=img3.fonwall.ru
+//https://image.fonwall.ru/o/vw/table-tablecloth-vase.jpeg?auto=compress&fit=resize&w=1200&h=1500&display=large&domain=img3.fonwall.ru
+//https://image.fonwall.ru/o/gq/landscape-nature-green-hill.jpeg?auto=compress&fit=resize&w=1200&h=686&display=large&domain=img3.fonwall.ru
